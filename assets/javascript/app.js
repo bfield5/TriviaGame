1464426096963edac61b58ce16573b5255a8ae8f
Brian Field <brianfield74@yahoo.com>
2018-08-09 23:30:39 -0600

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+
+var questionCounter = 0;
+var qTime = 15;
+var 
+
+
+
+
+
+
+ // question & answer array
+ var questions = [
+    {
+      question: "The animated series takes place in which state?",
+      choices: ["Washington", "Colorado", "California", "Montana"],
+      correctAnswer: "Colorado",
+      
+    }, 
+    {
+      question: "The character famous for dying each episode is??",
+      choices: ["Cartman", "Kyle", "Kenny", "Stan"],
+      correctAnswer: "Kenny"
+      
+    }, 
+    {
+      question: "After Kenny died for good, this character was the gang's first replacement?",
+      choices: ["Token", "Butters", "Jimmy", "Timmy"],
+      correctAnswer: ""
+      
+    }, 
+    {
+      question: "Cartman discovers the identity of his father and is shocked to learn it's?",
+      choices: ["Chef", "Big Gay Al", "His Mother", "Mr. Garrison"],
+      correctAnswer: ""
+      
+    }, 
+    {
+      question: "True or False: Kyle is in love with Wendy and vomits whenever she speaks to him",
+      choices: ["True", "False"],
+      correctAnswer: "True"
+      
+    },
+    {
+      question: "In the episode Cartman gets an Anal Probe who was taken by aliens?",
+      choices: ["Stan", "Shelly", "Cartman", "Ike"],
+      correctAnswer: ""
+      
+    },
+    {
+      question: "What cartoon is the gang's favorite?",
+      choices: ["Tom and Jerry", "Ichy and Scratchy", "Terrance and Phillip", "The Simpsons"],
+      correctAnswer: "Terrance and Phillip"
+      
+    },
+    {
+      question: "In the episode Chicken lover, Officer Barbrady is removed from duty because he can't read. Who takes his place?",
+      choices: ["Chef", "Cartman", "Mr. Garrison", "Stan's Uncle Jimbo"],
+      correctAnswer: ""
+      
+    },
+    {
+      question: "Who is the school's counselor, mmkay?",
+      choices: ["Mr. Garrison", "Mr. Mackey", "Mrs. Crabtree", "PC Principle"],
+      correctAnswer: "Mr. Mackey"
+      
+    }
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
